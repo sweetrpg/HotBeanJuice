e99b335c6a7b748da306d0f4d1329d737b351ef9
Paul Schifferer <paul@schifferers.net>
2024-01-30 16:17:35 -0800

    Make some coffee items compostable.

--- src/main/java/com/sweetrpg/hotbeanjuice/common/CommonSetup.java
+++ src/main/java/com/sweetrpg/hotbeanjuice/common/CommonSetup.java
@@ -1,7 +1,9 @@
 package com.sweetrpg.hotbeanjuice.common;
 
 import com.sweetrpg.hotbeanjuice.common.network.PacketHandler;
+import com.sweetrpg.hotbeanjuice.common.registry.ModItems;
 import com.sweetrpg.hotbeanjuice.common.world.gen.WildCropGeneration;
+import net.minecraft.world.level.block.ComposterBlock;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 public class CommonSetup {
@@ -14,6 +16,7 @@ public class CommonSetup {
 
             WildCropGeneration.registerWildCatnipGeneration();
 
+            registerCompostables();
         });
     }
 
@@ -21,5 +24,11 @@ public class CommonSetup {
 //
 //    }
 
-
+    public static void registerCompostables() {
+        ComposterBlock.COMPOSTABLES.put(ModItems.WILD_COFFEE_BUSH.get(), 0.65F);
+        ComposterBlock.COMPOSTABLES.put(ModItems.COFFEE_SEEDS.get(), 0.65F);
+        ComposterBlock.COMPOSTABLES.put(ModItems.COFFEE_CHERRY.get(), 1.0F);
+        ComposterBlock.COMPOSTABLES.put(ModItems.COFFEE_GROUNDS.get(), 1.5F);
+        ComposterBlock.COMPOSTABLES.put(ModItems.COFFEE_BEAN.get(), 1.0F);
+    }
 }
