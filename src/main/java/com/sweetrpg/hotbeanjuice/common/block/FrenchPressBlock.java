689aed8f5536d8670ae6810476b8b08e96aa0489
Paul Schifferer <paul@schifferers.net>
2024-12-19 21:02:42 -0800

    Checkpoint

--- src/main/java/com/sweetrpg/hotbeanjuice/common/block/FrenchPressBlock.java
+++ src/main/java/com/sweetrpg/hotbeanjuice/common/block/FrenchPressBlock.java
@@ -1,24 +1,37 @@
 package com.sweetrpg.hotbeanjuice.common.block;
 
 import com.sweetrpg.hotbeanjuice.common.block.entity.FrenchPressBlockEntity;
+import com.sweetrpg.hotbeanjuice.common.lib.CoffeeType;
 import com.sweetrpg.hotbeanjuice.common.registry.ModBlockEntityTypes;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
 
 public class FrenchPressBlock extends AbstractCoffeeMakerBlock {
+    public static final EnumProperty<CoffeeType> GROUNDS = EnumProperty.create("grounds", CoffeeType.class);
+    public static final BooleanProperty HAS_WATER = BooleanProperty.create("has_water");
+    public static final IntegerProperty STRENGTH = IntegerProperty.create("strength", 0, 50);
+
+    protected static final VoxelShape SHAPE = Block.box(4.0D, 0.0D, 4.0D, 12.0D, 13.0D, 12.0D);
 
     public FrenchPressBlock(Properties properties) {
         super(properties);
@@ -29,18 +42,31 @@ public class FrenchPressBlock extends AbstractCoffeeMakerBlock {
         return RenderShape.MODEL;
     }
 
+    @SuppressWarnings("deprecation")
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter worldIn, BlockPos pos, CollisionContext context) {
+        return SHAPE;
+    }
+
     @Override
     public @NotNull InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
+        if(hand == InteractionHand.MAIN_HAND) {
+            return InteractionResult.PASS;
+        }
+
         ItemStack itemStack = player.getItemInHand(hand);
         BlockEntity blockEntity = level.getBlockEntity(pos);
 
-        // TODO
+        // TODO: if the player is holding hot water, fill the press
+        // TODO: if the player is holding coffee grounds, add them to the press
+        // TODO: if the player's hand is empty, press the coffee; the strength of the coffee depends on the amount of time seeping
 
         return InteractionResult.sidedSuccess(level.isClientSide());
     }
 
     @Override
     public void playerWillDestroy(Level level, BlockPos pos, BlockState state, Player player) {
+        // TODO: store the contents, water level, and seep time
     }
 
     @Nullable
@@ -54,4 +80,5 @@ public class FrenchPressBlock extends AbstractCoffeeMakerBlock {
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> blockEntityType) {
         return createTickerHelper(blockEntityType, ModBlockEntityTypes.FRENCH_PRESS_BLOCK_ENTITY.get(), FrenchPressBlockEntity::tick);
     }
+
 }
--- src/main/java/com/sweetrpg/hotbeanjuice/common/lib/CoffeeType.java
+++ src/main/java/com/sweetrpg/hotbeanjuice/common/lib/CoffeeType.java
@@ -1,7 +1,15 @@
 package com.sweetrpg.hotbeanjuice.common.lib;
 
-public enum CoffeeType {
+import net.minecraft.util.StringRepresentable;
+
+public enum CoffeeType implements StringRepresentable {
     ARABICA,
     CANEPHORA,
     RACEMOSA,
+    ;
+
+    @Override
+    public String getSerializedName() {
+        return this.name();
+    }
 }
